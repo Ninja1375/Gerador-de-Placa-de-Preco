7c97bb935853ffb4bac2a3cdd1d192db82fbd495
Antônio Nascimento <166745758+Ninja1375@users.noreply.github.com>
2024-08-28 18:30:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,35 @@
 function updatePricePlate() {
- const promo = document.getElementById(
-  'promo').value;
- const productType = document
-  .getElementById('productType').value;
- const product = document
-  .getElementById('product').value;
- const extra = document.getElementById(
-  'extra').value;
- const price = document.getElementById(
-  'price').value;
- const unit = document.getElementById(
-  'unit').value;
+    const promo = document.getElementById('promo').value;
+    const productType = document.getElementById('productType').value;
+    const product = document.getElementById('product').value;
+    const extra = document.getElementById('extra').value;
+    const price = document.getElementById('price').value;
+    const unit = document.getElementById('unit').value;
 
- const pricePattern = /^d{1,3},d{2}$/;
+    const pricePattern = /^\d{1,3},\d{2}$/;
 
- if (!pricePattern.test(price)) {
-  alert(
-   "Por favor, insira o preço no formato 99,99"
-  );
-  return;
- }
+    if (!pricePattern.test(price)) {
+        alert("Por favor, insira o preço no formato 99,99");
+        return;
+    }
 
- document.getElementById('promoText')
-  .textContent = promo;
- document.getElementById(
-   'productTypeText').textContent =
-  productType;
- document.getElementById('productText')
-  .textContent = product;
- document.getElementById('extraText')
-  .textContent = extra;
- document.getElementById('priceText')
-  .textContent = price;
- document.getElementById('unitText')
-  .textContent = unit;
+    document.getElementById('promoText').textContent = promo;
+    document.getElementById('productTypeText').textContent = productType;
+    document.getElementById('productText').textContent = product;
+    document.getElementById('extraText').textContent = extra;
+    document.getElementById('priceText').textContent = price;
+    document.getElementById('unitText').textContent = unit;
 }
 
 function downloadImage() {
- html2canvas(document.getElementById(
-  'pricePlate')).then(canvas => {
-  let link = document.createElement(
-   'a');
-  link.download =
-   'placa_de_preco.png';
-  link.href = canvas.toDataURL(
-   'image/png');
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
- }).catch(err => {
-  console.error(
-   "Erro ao gerar a imagem: ", err);
- });
+    html2canvas(document.getElementById('pricePlate')).then(canvas => {
+        let link = document.createElement('a');
+        link.download = 'placa_de_preco.png';
+        link.href = canvas.toDataURL('image/png');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }).catch(err => {
+        console.error("Erro ao gerar a imagem: ", err);
+    });
 }
